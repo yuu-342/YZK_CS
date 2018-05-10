7e4dc25e0c600dd64ddf9be3651a932903d66a81
yuya <siren.night43@gmail.com>
2018-05-11 06:44:39 +0900

    header編集及びサイト説明追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
-  #  get '/news', to: 'static_pages#news'
+  get '/expo', to: 'static_pages#expo'
   #  get '/about', to: 'static_pages#about'
   get '/company', to: 'static_pages#company'
   get '/recruit', to: 'static_pages#recruit'
