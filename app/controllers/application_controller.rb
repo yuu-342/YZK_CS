78b83377bd460a8e0ee6a3f1a360645dd850fe5a
froover <dio22038@gmail.com>
2018-05-13 10:32:43 +0900

    Revert "image_pathを外してみる"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_request_variant
-
-  def index
-    # example
-    request.user_agent       #=> "Mozilla/5.0 (Linux; Android 4.3; Nexus 7 ..."
-    request.device_type      #=> :smartphone
-    request.os               #=> "Android"
-    request.os_version       #=> "4.3"
-    request.browser          #=> "Chrome"
-    request.browser_version  #=> "29.0.1547.72"
-    request.from_pc?         #=> false
-    request.from_smartphone? #=> true
-    request.from_tablet?
-  end
-
-  private
-
-  # Action Pack Variants向け
-  def set_request_variant
-    request.variant = request.device_type # :pc, :smartphone, etc
-  end
 end
